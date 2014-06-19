1d96889044e63664241edf26cb9e578327687f0a
Lukasz Olszewski <asdluki@gmail.com>
2014-06-20 01:57:13 +0200

    add.sh fixed

--- deployer/add.sh
+++ deployer/add.sh
@@ -1,3 +1,3 @@
 
-mvn install:install-file -Dmaven.repo.local=../releases -Dfile=libGoogleAnalyticsServices.jar -Dsources=google-play-services.jar -DgroupId=com.google.android.gms -DartifactId=google-play-services -Dversion=1.0.0 -Dpackaging=jar -DcreateChecksum=true
+mvn install:install-file -Dmaven.repo.local=../releases -Dfile=google-play-services.jar -DgroupId=com.google.android.gms -DartifactId=google-play-services -Dversion=1.0.0 -Dpackaging=jar -DcreateChecksum=true
 
