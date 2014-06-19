0fcb587a36bcce4a555d0efa086aed542d2b2c61
Lukasz Olszewski <asdluki@gmail.com>
2014-06-20 01:34:31 +0200

    add: gcm,gms,analitycs

---
+++ deployer/add.sh
@@ -0,0 +1,3 @@
+
+mvn install:install-file -Dmaven.repo.local=../releases -Dfile=libGoogleAnalyticsServices.jar -Dsources=libGoogleAnalyticsServices.jar -DgroupId=com.google -DartifactId=google-analytic-services -Dversion=1.0.0 -DpomFile=default.pom -Dpackaging=jar -DcreateChecksum=true
+
